006914a97754f5ee536c5eee988c138f7c7ff36b
sieve <68281563+s-i-e-v-e@users.noreply.github.com>
2020-08-11 01:14:00 +0000

    Remove vm

--- src/runner.ts
+++ src/runner.ts
@@ -12,8 +12,6 @@ import {
     Errors,
     OS,
 } from "./util/mod.ts";
-import { vm_gen_code } from "./codegen/mod.ts";
-import { Vm } from "./vm/mod.ts";
 
 export interface Config {
     logLevel: number,
@@ -25,12 +23,6 @@ async function runFile(path: string, args: string[], c: Config) {
     const mods = await parseFile(c.base, path);
     const global = check(mods);
     rewrite(global, mods);
-    const vme = vm_gen_code(mods);
-    const xs = vme.asBytes();
-    if (c.dump) OS.writeBinaryFile("./dump.bin", xs);
-    Logger.debug("@@--------VM.EXEC--------@@");
-    const vm = Vm.build(vme.importsOffset);
-    vm.exec(xs, args);
 }
 
 export async function run(args: string[], c: Config) {
